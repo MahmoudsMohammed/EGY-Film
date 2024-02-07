fb952e11b68891343c759de1da636623e99be2c3
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-02-07 14:20:07 +0200

    make Authentication using firebase

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -2,17 +2,39 @@ declare var google: any;
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { authService } from './auth.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
 export class AuthComponent implements OnInit {
   constructor(private router: Router, private authServ: authService) {}
+
+  form: FormGroup;
+  signIn = true;
+  isError = '';
+
   ngOnInit(): void {
+    // form
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      pass: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+    // Google
     google.accounts.id.initialize({
       client_id:
         '638827466645-v485amo6q5l59vjsqanpqndjakrlh5o6.apps.googleusercontent.com',
@@ -24,7 +46,7 @@ export class AuthComponent implements OnInit {
       theme: 'filled_black',
       size: 'large',
       shape: 'rectangle',
-      width: 300,
+      width: 280,
     });
   }
 
@@ -32,6 +54,51 @@ export class AuthComponent implements OnInit {
     const userData = JSON.parse(atob(res.split('.')[1]));
     sessionStorage.setItem('userData', JSON.stringify(userData));
     this.authServ.user.next(userData);
-    this.router.navigate(['home']);
+    this.router.navigate(['/home']);
+  }
+
+  // submit form
+  onSubmit() {
+    if (!this.signIn) {
+      // SignUp
+      this.authServ
+        .SignUp(this.form.value.email, this.form.value.pass)
+        .subscribe(
+          (res) => {
+            let name = this.form.value.name as string;
+            res['name'] = name[0].toUpperCase() + name.slice(1, name.length);
+            sessionStorage.setItem('userData', JSON.stringify(res));
+            this.authServ.user.next(res);
+            this.router.navigate(['/home']);
+          },
+          (error) => {
+            this.isError = error;
+            this.disappearError();
+          }
+        );
+    } else {
+      // SignIn
+      this.authServ
+        .SignIn(this.form.value.email, this.form.value.pass)
+        .subscribe(
+          (res) => {
+            let name = this.form.value.name as string;
+            res['name'] = name[0].toUpperCase() + name.slice(1, name.length);
+            sessionStorage.setItem('userData', JSON.stringify(res));
+            this.authServ.user.next(res);
+            this.router.navigate(['/home']);
+          },
+          (error) => {
+            this.isError = error;
+            this.disappearError();
+          }
+        );
+    }
+  }
+
+  disappearError() {
+    setTimeout(() => {
+      this.isError = '';
+    }, 2000);
   }
 }
--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, catchError, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class authService {
+  constructor(private http: HttpClient) {}
   user = new BehaviorSubject<object>(
     JSON.parse(sessionStorage.getItem('userData')) ?? null
   );
+
+  // sign up
+  SignUp(email, password) {
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBoNz1kD1Dm7CXOqzFtxTwg8alLtrZWRc8`,
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(catchError(this.errorHandler));
+  }
+
+  // sign in
+  SignIn(email, password) {
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBoNz1kD1Dm7CXOqzFtxTwg8alLtrZWRc8`,
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    let message = 'There is unknown error please try later.';
+    if (!error.error.error) {
+      message = 'Check Your Internet Connection.';
+    } else {
+      switch (error.error.error.message) {
+        case 'EMAIL_EXISTS':
+          message = 'The email address is already in use by another account.';
+          break;
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+          message =
+            'We have blocked all requests from this device due to unusual activity. Try again later.';
+          break;
+        case 'EMAIL_NOT_FOUND' || 'INVALID_EMAIL':
+          message = 'There is no user Signup First.';
+          break;
+        case 'INVALID_PASSWORD':
+          message = 'The password is invalid.';
+          break;
+      }
+    }
+    return throwError(message);
+  }
 }
