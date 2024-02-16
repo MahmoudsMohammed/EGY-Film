307ce4b3bb46ca7580ee6fc9a04c25fe329fff04
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-02-16 16:06:37 +0200

    async google request

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -29,6 +29,25 @@ export class AuthComponent implements OnInit {
   isError = '';
 
   ngOnInit(): void {
+    // Google
+    // id hosting
+    // 1002217737761-jd0ggvvqpknlmj4u20eicqf0e3rave2c.apps.googleusercontent.com
+    // id local
+    // 638827466645-v485amo6q5l59vjsqanpqndjakrlh5o6.apps.googleusercontent.com
+    setTimeout(() => {
+      google.accounts.id.initialize({
+        client_id:
+          '1002217737761-jd0ggvvqpknlmj4u20eicqf0e3rave2c.apps.googleusercontent.com',
+        callback: (res) =>
+          this.ngZone.run(() => this.googleData(res.credential)),
+      });
+      google.accounts.id.renderButton(document.getElementById('google-btn'), {
+        theme: 'filled_black',
+        size: 'large',
+        shape: 'rectangle',
+        width: 280,
+      });
+    }, 1000);
     // form
     this.form = new FormGroup({
       name: new FormControl(null, [Validators.required]),
@@ -38,24 +57,6 @@ export class AuthComponent implements OnInit {
         Validators.minLength(6),
       ]),
     });
-    // Google
-    // id hosting
-    // 1002217737761-jd0ggvvqpknlmj4u20eicqf0e3rave2c.apps.googleusercontent.com
-    // id local
-    // 638827466645-v485amo6q5l59vjsqanpqndjakrlh5o6.apps.googleusercontent.com
-    google.accounts.id.initialize({
-      client_id:
-        '638827466645-v485amo6q5l59vjsqanpqndjakrlh5o6.apps.googleusercontent.com',
-      callback: (res) => this.ngZone.run(() => this.googleData(res.credential)),
-    });
-    setTimeout(() => {
-      google.accounts.id.renderButton(document.getElementById('google-btn'), {
-        theme: 'filled_black',
-        size: 'large',
-        shape: 'rectangle',
-        width: 280,
-      });
-    }, 1000);
   }
 
   googleData(res: any) {
